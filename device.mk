c1fdbb52c3bcc98471ea549192fb1dbfd13dde43
Guillaume La Roque <glaroque@baylibre.com>
2023-11-03 12:26:49 +0100

    yukawa: Clean up and rework
    - Remove deprecated platform : sei510 and sei610
    - Remove 32bits only support
    - Drop legacy (non A/B) support
    - Support kernel 6.1 only to prepare full GKI support
    - Cosmetic rework
    - Use Broadcom upstream driver with new firmware shipped with vendor
      package
    - Use Codec2 instead of OMX service
    - Remove deprecated HAL and move on generic implementation when it's
      possible
    - Fix factory reset
    - Fix Black UI when we boot Android TV variant.
    
    This commit is to prepare more important update to move on last Shipping
    api and manifest level and so support of last requirements of Android.
    
    Change-Id: I949993976a5111f4649b80ac30a60d794c857fc9
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
    Signed-off-by: Maxime Fournier <mfournier@baylibre.com>

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Packages to invoke RC pairing
-PRODUCT_PACKAGES += YukawaService YukawaAndroidOverlay
+PRODUCT_CHARACTERISTICS := tablet
 else
 # Setup TV Build
 USE_OEM_TV_APP := true
@@ -61,47 +61,33 @@ endif
 $(call inherit-product, $(YUKAWA_VENDOR_PATH)/bt-wifi-firmware/$(EXPECTED_YUKAWA_VENDOR_VERSION)/vendor.mk)
 $(call inherit-product, $(YUKAWA_VENDOR_PATH)/video_firmware/$(EXPECTED_YUKAWA_VENDOR_VERSION)/vendor.mk)
 
-PRODUCT_PACKAGES += llkd
-
-ifeq ($(TARGET_USE_AB_SLOT), true)
 # A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
-    update_verifier
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALm=true \
-    POSTINSTALL_PATH=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE=ext4 \
-    POSTINSTALL_OPTIONAL=true
-
-PRODUCT_PACKAGES += \
     update_engine_sideload \
+    update_verifier \
     sg_write_buffer \
-    f2fs_io
+    f2fs_io \
+    check_f2fs
 
 # The following modules are included in debuggable builds only.
 PRODUCT_PACKAGES_DEBUG += \
     bootctl \
-    update_engine_client
+    update_engine_client \
+    SystemUpdaterSample
 
-# Write flags to the vendor space in /misc partition.
+# Userdata Checkpointing OTA GC
 PRODUCT_PACKAGES += \
-    misc_writer
-
-PRODUCT_PACKAGES += \
-    fs_config_dirs \
-    fs_config_files
+	checkpoint_gc
 
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service \
-    bootctrl.yukawa.recovery \
-    bootctrl.yukawa
-endif
+    bootctrl.default
 
 # System RO FS Type
 TARGET_RO_FILE_SYSTEM_TYPE ?= ext4
@@ -116,39 +102,23 @@ PRODUCT_PACKAGES += \
 	android.hardware.fastboot@1.0-impl-mock \
 	fastbootd
 
-# All VNDK libraries (HAL interfaces, VNDK, VNDK-SP, LL-NDK)
-PRODUCT_PACKAGES += vndk_package
 
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-cuttlefish \
-    android.hardware.health@2.1-service
-
-ifeq ($(TARGET_USE_AB_SLOT), true)
 ifeq ($(TARGET_AVB_ENABLE), true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.yukawa.avb.ab:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.yukawa \
-    $(LOCAL_PATH)/fstab.yukawa.avb.ab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa
+    $(LOCAL_PATH)/fstab.yukawa.avb.ab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa \
+    $(LOCAL_PATH)/fstab.yukawa.avb.ab:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/etc/fstab.yukawa
 else
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.yukawa.ab:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.yukawa \
-    $(LOCAL_PATH)/fstab.yukawa.ab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa
-endif
-else
-ifeq ($(TARGET_AVB_ENABLE), true)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.ramdisk.common.avb:$(TARGET_COPY_OUT_RAMDISK)/fstab.yukawa \
-    $(LOCAL_PATH)/fstab.yukawa:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa
-else
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.ramdisk.common:$(TARGET_COPY_OUT_RAMDISK)/fstab.yukawa \
-    $(LOCAL_PATH)/fstab.yukawa:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa
-endif
+    $(LOCAL_PATH)/fstab.yukawa.ab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.yukawa \
+    $(LOCAL_PATH)/fstab.yukawa.ab:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/etc/fstab.yukawa
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.yukawa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yukawa.rc \
     $(LOCAL_PATH)/init.yukawa.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.yukawa.usb.rc \
-    $(LOCAL_PATH)/init.recovery.hardware.rc:recovery/root/init.recovery.yukawa.rc \
+    $(LOCAL_PATH)/init.recovery.hardware.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.yukawa.rc \
     $(LOCAL_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
     $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
@@ -158,11 +128,6 @@ ifeq ($(TARGET_USE_TABLET_LAUNCHER), true)
 # Use Launcher3QuickStep
 PRODUCT_PACKAGES += Launcher3QuickStep
 else
-ifeq ($(TARGET_USE_SAMPLE_LAUNCHER), true)
-PRODUCT_PACKAGES += \
-    TvSampleLeanbackLauncher
-endif
-
 # TV Specific Packages
 PRODUCT_PACKAGES += \
     LiveTv \
@@ -172,20 +137,18 @@ PRODUCT_PACKAGES += \
     com.android.media.tv.remoteprovider \
     InputDevices
 
+# Fallback IME and Home apps
 PRODUCT_PACKAGES += \
-    LeanbackIME
+    LeanbackIME \
+    TvSampleLeanbackLauncher
 
-ifeq (,$(filter $(TARGET_PRODUCT),yukawa_gms yukawa32_gms yukawa_sei510_gms))
+ifeq ($(TARGET_PRODUCT), yukawa_gms)
 PRODUCT_PACKAGES += \
     TvProvision \
     TVLauncherNoGms \
     TVRecommendationsNoGms
 endif
 endif
-
-PRODUCT_PACKAGES += \
-    libhidltransport \
-    libhwbinder
 
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 
@@ -249,9 +212,20 @@ PRODUCT_PACKAGES += \
 # Hardware Composer HAL
 #
 PRODUCT_PACKAGES += \
-    hwcomposer.drm_meson \
+    hwcomposer.drm_meson
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.hwcomposer=drm_meson \
+    ro.hardware.egl=mali \
+    ro.hardware.vulkan=yukawa
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.gralloc=yukawa
+
+# DRM Service
+PRODUCT_PACKAGES += \
     android.hardware.drm-service.widevine \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@latest-service.clearkey
 
 # CEC
 PRODUCT_PACKAGES += \
@@ -282,6 +256,11 @@ PRODUCT_PACKAGES += \
 # PowerStats HAL
 PRODUCT_PACKAGES += \
     android.hardware.power.stats-service.example
+
+# Health: Install default binderized implementation to vendor.
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-cuttlefish \
+    android.hardware.health@2.1-service
 
 # Sensor HAL
 ifneq ($(TARGET_SENSOR_MEZZANINE),)
@@ -361,35 +340,19 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
-AUDIO_DEFAULT_OUTPUT ?= speaker
-ifeq ($(AUDIO_DEFAULT_OUTPUT),hdmi)
 PRODUCT_COPY_FILES += \
     device/amlogic/yukawa/hal/audio/mixer_paths_hdmi_only.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     device/amlogic/yukawa/hal/audio/audio_policy_configuration_hdmi_only.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 DEVICE_PACKAGE_OVERLAYS += \
     device/amlogic/yukawa/hal/audio/overlay_hdmi_only
 TARGET_USE_HDMI_AUDIO ?= true
-else
-PRODUCT_COPY_FILES += \
-    device/amlogic/yukawa/hal/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    device/amlogic/yukawa/hal/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
-endif
 
 # Copy media codecs config file
 PRODUCT_COPY_FILES += \
-    device/amlogic/yukawa/media_xml/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    device/amlogic/yukawa/media_xml/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml
-
-# Enable BT Pairing with button BTN_0 (key 256)
-
-PRODUCT_COPY_FILES += \
-    device/amlogic/yukawa/input/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
-
-# Light HAL
-PRODUCT_PACKAGES += \
-    android.hardware.light-service \
-    lights-yukawa
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    device/amlogic/yukawa/media_xml/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Enable USB Camera
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
@@ -399,3 +362,6 @@ PRODUCT_COPY_FILES += \
 
 # Include Virtualization APEX
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
+
+# ro.frp.pst points to a partition that contains factory reset protection information.
+PRODUCT_VENDOR_PROPERTIES += ro.frp.pst=/dev/block/by-name/frp
--- yukawa.mk
+++ yukawa.mk
@@ -1,14 +1,59 @@
 # Inherit the full_base and device configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, device/amlogic/yukawa/device-yukawa.mk)
-$(call inherit-product, device/amlogic/yukawa/yukawa-common.mk)
+ifndef TARGET_KERNEL_USE
+TARGET_KERNEL_USE := 6.1
+endif
 
+ifeq ($(TARGET_VIM3), true)
+TARGET_DEV_BOARD := vim3
+else ifeq ($(TARGET_VIM3L), true)
+TARGET_DEV_BOARD := vim3l
+else ifeq ($(TARGET_DEV_BOARD),)
+TARGET_DEV_BOARD := vim3l
+endif
+
+ifneq ($(filter $(TARGET_DEV_BOARD),vim3),)
+GPU_TYPE := gondul_ion
+endif
+
+$(call inherit-product, device/amlogic/yukawa/device.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += ro.product.device=$(TARGET_DEV_BOARD)
+GPU_TYPE ?= dvalin_ion
+
+BOARD_KERNEL_DTB := device/amlogic/yukawa-kernel/$(TARGET_KERNEL_USE)
+
+ifeq ($(TARGET_PREBUILT_DTB),)
+LOCAL_DTB := $(BOARD_KERNEL_DTB)
+else
+LOCAL_DTB := $(TARGET_PREBUILT_DTB)
+endif
+
+# Feature permissions
+PRODUCT_COPY_FILES += \
+    device/amlogic/yukawa/permissions/yukawa.xml:/system/etc/sysconfig/yukawa.xml
+
+
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+# Enforce the Product interface
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
+ifeq ($(TARGET_USE_TABLET_LAUNCHER), true)
+PRODUCT_MODEL := Android Tablet on yukawa
+else
+PRODUCT_MODEL := ATV on yukawa
+endif
+
+PRODUCT_BRAND := Android
+PRODUCT_MANUFACTURER := KHADAS
 PRODUCT_NAME := yukawa
 PRODUCT_DEVICE := yukawa
 
-ifndef TARGET_KERNEL_USE
-TARGET_KERNEL_USE := 5.10
-endif
+# Set SOC information
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
+    ro.soc.model=$(PRODUCT_DEVICE)
 
 MOD_DIR := device/amlogic/yukawa-kernel/$(TARGET_KERNEL_USE)
 
@@ -19,9 +64,10 @@ BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(MOD_DIR)/*.ko)
 
 ifneq ($(BOARD_VENDOR_KERNEL_MODULES),)
 
-#
-# ...and only a subset on the ramdisk.
-#
+# serial
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/meson_uart.ko
+
 # core clock providers
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
   $(MOD_DIR)/axg.ko \
@@ -60,9 +106,35 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
 
 # misc.
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/system_heap.ko \
+  $(MOD_DIR)/cma_heap.ko \
   $(MOD_DIR)/meson-ee-pwrc.ko \
   $(MOD_DIR)/pwm-meson.ko \
-  $(MOD_DIR)/pwm-regulator.ko
+  $(MOD_DIR)/pwm-regulator.ko \
+  $(MOD_DIR)/meson-rng.ko \
+  $(MOD_DIR)/meson_sm.ko \
+  $(MOD_DIR)/meson-secure-pwrc.ko \
+  $(MOD_DIR)/meson_wdt.ko \
+  $(MOD_DIR)/meson-clk-measure.ko \
+  $(MOD_DIR)/meson-gx-pwrc-vpu.ko \
+  $(MOD_DIR)/meson_gxbb_wdt.ko \
+  $(MOD_DIR)/meson-ir.ko \
+  $(MOD_DIR)/meson_saradc.ko \
+  $(MOD_DIR)/dwc3-meson-g12a.ko \
+  $(MOD_DIR)/rtc-meson-vrtc.ko \
+  $(MOD_DIR)/pcs_xpcs.ko \
+  $(MOD_DIR)/stmmac.ko \
+  $(MOD_DIR)/stmmac-platform.ko \
+  $(MOD_DIR)/dwmac-meson.ko \
+  $(MOD_DIR)/dwmac-meson8b.ko \
+  $(MOD_DIR)/pci-meson.ko \
+  $(MOD_DIR)/irq-meson-gpio.ko \
+  $(MOD_DIR)/mdio-mux.ko \
+  $(MOD_DIR)/mdio-mux-meson-g12a.ko \
+  $(MOD_DIR)/meson-gxl.ko \
+  $(MOD_DIR)/spi-meson-spicc.ko \
+  $(MOD_DIR)/spi-meson-spifc.ko
+
 
 # SD/eMMC
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
@@ -70,29 +142,56 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
   $(MOD_DIR)/pwrseq_simple.ko \
   $(MOD_DIR)/pwrseq_emmc.ko
 
-#
-# ...and only a subset of those to explicitly load, mainly to get
-# SD/eMMC up so the main rootfs can be loaded
-#
-# NOTE: this list is G12/SM1 specific
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
-  $(MOD_DIR)/g12a_aoclk.ko \
-  $(MOD_DIR)/g12a.ko \
-  $(MOD_DIR)/meson-eeclk.ko \
-  $(MOD_DIR)/pinctrl-meson-g12a.ko \
-  $(MOD_DIR)/reset-meson.ko \
-  $(MOD_DIR)/pwm-meson.ko \
-  $(MOD_DIR)/pwrseq_simple.ko \
-  $(MOD_DIR)/pwrseq_emmc.ko \
-  $(MOD_DIR)/meson-gx-mmc.ko
+# i2c
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/i2c-meson.ko \
 
-#
-# serial console (may be built-in, so check if present)
-#
-UART_MOD=$(MOD_DIR)/meson_uart.ko
-ifneq (,$(wildcard $(UART_MOD)))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(UART_MOD)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += $(UART_MOD)
-endif
+# Phy
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/phy-meson-axg-mipi-pcie-analog.ko \
+  $(MOD_DIR)/phy-meson-axg-pcie.ko \
+  $(MOD_DIR)/phy-meson-g12a-usb2.ko \
+  $(MOD_DIR)/phy-meson-g12a-usb3-pcie.ko \
+  $(MOD_DIR)/phy-meson-gxl-usb2.ko \
+  $(MOD_DIR)/phy-meson8b-usb2.ko
+
+# Audio
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/snd-soc-meson-aiu.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-fifo.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-frddr.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-sound-card.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-spdifout.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-pdm.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-tdm-formatter.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-tdmout.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-spdifin.ko \
+  $(MOD_DIR)/snd-soc-meson-card-utils.ko \
+  $(MOD_DIR)/snd-soc-meson-codec-glue.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-tdm-interface.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-tdmin.ko \
+  $(MOD_DIR)/snd-soc-meson-gx-sound-card.ko \
+  $(MOD_DIR)/snd-soc-meson-t9015.ko \
+  $(MOD_DIR)/snd-soc-meson-axg-toddr.ko \
+  $(MOD_DIR)/snd-soc-meson-g12a-toacodec.ko \
+  $(MOD_DIR)/snd-soc-meson-g12a-tohdmitx.ko
+
+# Video/GPU
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+  $(MOD_DIR)/ao-cec.ko \
+  $(MOD_DIR)/ao-cec-g12a.ko \
+  $(MOD_DIR)/mali_kbase.ko \
+  $(MOD_DIR)/display-connector.ko \
+  $(MOD_DIR)/drm_display_helper.ko \
+  $(MOD_DIR)/drm_dma_helper.ko \
+  $(MOD_DIR)/dw-hdmi.ko \
+  $(MOD_DIR)/dw-hdmi-cec.ko \
+  $(MOD_DIR)/dw-hdmi-i2s-audio.ko \
+  $(MOD_DIR)/dw-hdmi-ahb-audio.ko \
+  $(MOD_DIR)/meson-canvas.ko \
+  $(MOD_DIR)/meson-drm.ko \
+  $(MOD_DIR)/meson_dw_hdmi.ko
+
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES)
 
 endif
