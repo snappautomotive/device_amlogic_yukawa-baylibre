bf2a6df69c2dff7db3f442ecef20bcc658000e80
Al Sutton <al.sutton@alsutton.com>
2024-11-14 08:53:55 +0000

    Remove deprecated TARGET_USES_64_BIT_BINDER usage
    This was deprecated in [1] which is part of QPR2
    
    [1] https://android.googlesource.com/platform/build/+/52fbc7df01551337602adf9e71da2c0df184d813%5E%21/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-# Required flag for non-64 bit devices from P.
-TARGET_USES_64_BIT_BINDER := true
-
 # 64 bit mediadrmserver
 TARGET_ENABLE_MEDIADRM_64 := true
 
